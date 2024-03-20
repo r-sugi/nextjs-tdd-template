df4f2becd3ddc801f8eaeb4f0284865bb69a6a1e
r-sugi <r.sugimoto0615@gmail.com>
2024-03-20 21:53:53 +0900

    fix;

--- src/components/templates/Posts/PostId/PostId.tsx
+++ src/components/templates/Posts/PostId/PostId.tsx
@@ -3,21 +3,17 @@ import { Post } from "__fixtures__/posts/post.type";
 import { FC } from "react";
 import { PostIdErrorBoundary } from "@/components/error/custom/PostIdErrorBoundary";
 import { PostIdErrorScreen } from "@/components/error/screen/PostIdErrorScreen";
-
-// メモ: client側処理
-// 検査例外(ビジネスロジックとしてエラーハンドリングできるもの)、非検査例外(ビジネスロジック外のエラー)でErrorBoundaryを使い分ける
-// type: Error はビジネスロジック。 NotificationBar などで通知
-// type: Critical はビジネスロジック。 全体 UI 表示。 HandleableErrorBoundary
-// type: Fatal は非ビジネスロジック（ネットワークエラーや Developer の実装ミス）。 全体 UI 表示。UnhandleabeErrorBoundary
+import { fetchPostById } from "@/repositories/post/postRepository";
 
 type Props = {
   post: Post;
 };
 
 export const PostId: FC<Props> = ({ post }) => {
+  // TODO: apollo or SWRにする
   // try {
-  //   // const postId = "2"; // 一旦固定値
-  //   // const res = await apiClient<Post>(`/post/${postId}`);
+  //   const postId = "2"; // 一旦固定値
+  //   const res = await fetchPostById(post.id);
   // } catch (error: unknown) {
   //   throw new ClientAppErrorTransformer().transform(error);
   // }
