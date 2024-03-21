e388a3819b084c1f33a49ad7de66d44d4867fb97
r-sugi <r.sugimoto0615@gmail.com>
2024-03-21 10:21:49 +0900

    add: swr

--- src/components/templates/Posts/PostId/PostId.tsx
+++ src/components/templates/Posts/PostId/PostId.tsx
@@ -3,20 +3,24 @@ import { Post } from "__fixtures__/posts/post.type";
 import { FC } from "react";
 import { PostIdErrorBoundary } from "@/components/error/baundary/PostIdErrorBoundary";
 import { PostIdErrorScreen } from "@/components/error/screen/PostIdErrorScreen";
-import { fetchPostById } from "@/repositories/post/postRepository";
-
+import { useFetchPostById } from "@/repositories/post/postRepository";
 type Props = {
   post: Post;
 };
 
 export const PostId: FC<Props> = ({ post }) => {
-  // TODO: apollo or SWRにする
-  // try {
-  //   const postId = "2"; // 一旦固定値
-  //   const res = await fetchPostById(post.id);
-  // } catch (error: unknown) {
-  //   throw new ClientAppErrorTransformer().transform(error);
-  // }
+  const { post: data, error, isLoading } = useFetchPostById(post.id);
+
+  if (error) {
+    // Boundaryでエラーを受け取って描画する想定のエラー対応
+    throw new ClientAppErrorTransformer().transform(error);
+  }
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
+  if (!data) {
+    return <div>no data</div>;
+  }
 
   return (
     <div>
--- src/repositories/post/postRepository.ts
+++ src/repositories/post/postRepository.ts
@@ -1,5 +1,6 @@
 import { Post } from "__fixtures__/posts/post.type";
 import { apiClient } from "@/lib/apiClient";
+import useSWR from "swr";
 
 export const fetchPostById = async (id: string) => {
   return await apiClient<Post>(`/api/posts/${id}`);
@@ -7,4 +8,18 @@ export const fetchPostById = async (id: string) => {
 
 export const fetchPosts = async () => {
   return await apiClient<Post[]>(`/api/posts`);
+};
+
+export const useFetchPostById = (id: string | number) => {
+  const {
+    data: post,
+    error,
+    isLoading,
+  } = useSWR(`/api/posts/${id}`, apiClient<Post>);
+
+  return {
+    post,
+    error,
+    isLoading,
+  };
 };
