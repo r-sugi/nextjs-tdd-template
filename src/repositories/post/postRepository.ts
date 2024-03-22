cffa841e260623af5df264eceb90454f9b0af69a
r-sugi <r.sugimoto0615@gmail.com>
2024-03-23 07:53:59 +0900

    fix: 不要な処理を削除した

--- src/repositories/post/postRepository.ts
+++ src/repositories/post/postRepository.ts
@@ -2,13 +2,13 @@ import { Post } from "__fixtures__/posts/post.type";
 import { apiClient } from "@/lib/apiClient";
 import useSWR from "swr";
 
-export const fetchPostById = async (id: string) => {
-  return await apiClient<Post>(`/api/posts/${id}`);
-};
+// export const fetchPostById = async (id: string) => {
+//   return await apiClient<Post>(`/api/posts/${id}`);
+// };
 
-export const fetchPosts = async () => {
-  return await apiClient<Post[]>(`/api/posts`);
-};
+// export const fetchPosts = async () => {
+//   return await apiClient<Post[]>(`/api/posts`);
+// };
 
 export const useFetchPostById = (id: string | number) => {
   const {
