db47fadb1356411047717db6467fae559d494aff
r-sugi <r.sugimoto0615@gmail.com>
2024-04-09 03:34:21 +0900

    fix: 不要なソースコード

--- src/components/templates/Posts/Posts.tsx
+++ src/components/templates/Posts/Posts.tsx
@@ -1,28 +1,24 @@
 import { FC } from "react";
-import { ErrorTransformer } from "../../../error/transformer/error.transformer";
-import { useGetTableTennisTablesQuery } from "@/generated/graphql";
 
 const PostsTemplate: FC<{}> = () => {
-  const { data, loading, error } = useGetTableTennisTablesQuery();
-
-  if (error) {
-    throw new ErrorTransformer().transform(error);
-  }
-  if (loading) {
-    return <div>loading...</div>;
-  }
-  if (data?.table_tennis_tables == null) {
-    return <div>no data</div>;
-  }
+  // if (error) {
+  //   throw new ErrorTransformer().transform(error);
+  // }
+  // if (loading) {
+  //   return <div>loading...</div>;
+  // }
+  // if (data?.table_tennis_tables == null) {
+  //   return <div>no data</div>;
+  // }
 
   return (
     <>
       <div>PostsTemplate</div>
-      <ul>
+      {/* <ul>
         {data?.table_tennis_tables.map((table) => (
           <li key={table.id}>{table.name}</li>
         ))}
-      </ul>
+      </ul> */}
     </>
   );
 };
