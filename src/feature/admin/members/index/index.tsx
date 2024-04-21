c76a1d8c3ca4ebe276d99f530242bf202c63273a
r-sugi <r.sugimoto0615@gmail.com>
2024-04-21 13:17:01 +0900

    fix: show user list by userStatus

--- src/feature/admin/members/index/index.tsx
+++ src/feature/admin/members/index/index.tsx
@@ -1,13 +1,13 @@
-import { fetchMembersByStatus } from "@/core/repositories/member/members.repository";
 import { useFetchMembers } from "@/core/usecases/member/useFetchMembers.query";
 import { FC } from "react";
+import { memberStatus } from "@/core/domains/member/status";
+import css from "styled-jsx/css";
 
 export const IndexTemplate: FC = () => {
-  // TODO: タブを切り替えたら、statusを変更してfetchする
-  const { members } = useFetchMembers({ status: "banned" });
+  const { members, setUserStatus, userStatus } = useFetchMembers();
 
   // if (error) {
-  //   throw new ErrorTransformer().transform(error);
+  //   throw new ErrorTranshtmlFormer().transhtmlForm(error);
   // }
   // if (isLoading) {
   //   return <div>loading...</div>;
@@ -16,15 +16,99 @@ export const IndexTemplate: FC = () => {
   //   return <div>no data</div>;
   // }
 
+  const memberListComponent = () => {
+    return (
+      <ul>
+        {members.map((member) => (
+          <li key={member.createdAt}>{JSON.stringify(member, null, 2)}</li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <div>
-      <ul>
-        {members.map((member) => {
-          return (
-            <li key={member.createdAt}>{JSON.stringify(member, null, 2)}</li>
-          );
-        })}
-      </ul>
+      {/* TODO: styleに関するリファクタ */}
+      <style jsx={true}>{`
+        .tab-wrap {
+          display: flex;
+          flex-wrap: wrap;
+        }
+        .tab-label {
+          color: White;
+          background: LightGray;
+          margin-right: 5px;
+          padding: 3px 12px;
+          order: -1;
+        }
+        .tab-content {
+          width: 100%;
+          display: none;
+        }
+        /* アクティブなタブ */
+        .tab-switch:checked + .tab-label {
+          background: DeepSkyBlue;
+        }
+        .tab-switch:checked + .tab-label + .tab-content {
+          display: block;
+        }
+        /* ラジオボタン非表示 */
+        .tab-switch {
+          display: none;
+        }
+      `}</style>
+      {/* TODO: componentsのリファクタ */}
+      {/* メンバー一覧 */}
+      <div className="tab-wrap">
+        <input
+          id={memberStatus.pendingActivation}
+          type="radio"
+          name="TAB"
+          className="tab-switch"
+          checked={userStatus === memberStatus.pendingActivation}
+          onChange={() => setUserStatus(memberStatus.pendingActivation)}
+        />
+        <label className="tab-label" htmlFor={memberStatus.pendingActivation}>
+          認証前
+        </label>
+        <div className="tab-content">{memberListComponent()}</div>
+        <input
+          id={memberStatus.active}
+          type="radio"
+          name="TAB"
+          className="tab-switch"
+          checked={userStatus === memberStatus.active}
+          onChange={() => setUserStatus(memberStatus.active)}
+        />
+        <label className="tab-label" htmlFor={memberStatus.active}>
+          アクティブ
+        </label>
+        <div className="tab-content">{memberListComponent()}</div>
+        <input
+          id={memberStatus.resigned}
+          type="radio"
+          name="TAB"
+          className="tab-switch"
+          checked={userStatus === memberStatus.resigned}
+          onChange={() => setUserStatus(memberStatus.resigned)}
+        />
+        <label className="tab-label" htmlFor={memberStatus.resigned}>
+          退会済み
+        </label>
+        <div className="tab-content">{memberListComponent()}</div>
+        <input
+          id={memberStatus.banned}
+          type="radio"
+          name="TAB"
+          className="tab-switch"
+          checked={userStatus === memberStatus.banned}
+          onChange={() => setUserStatus(memberStatus.banned)}
+        />
+        <label className="tab-label" htmlFor={memberStatus.banned}>
+          アカウント停止
+        </label>
+        <div className="tab-content">{memberListComponent()}</div>
+      </div>
     </div>
   );
 };
@@ -32,3 +116,32 @@ export const IndexTemplate: FC = () => {
 export const PostIdTemplate: FC = () => {
   return <IndexTemplate />;
 };
+
+const style = css`
+  .tab-wrap {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .tab-label {
+    color: White;
+    background: LightGray;
+    margin-right: 5px;
+    padding: 3px 12px;
+    order: -1;
+  }
+  .tab-content {
+    width: 100%;
+    display: none;
+  }
+  /* アクティブなタブ */
+  .tab-switch:checked + .tab-label {
+    background: DeepSkyBlue;
+  }
+  .tab-switch:checked + .tab-label + .tab-content {
+    display: block;
+  }
+  /* ラジオボタン非表示 */
+  .tab-switch {
+    display: none;
+  }
+`;
