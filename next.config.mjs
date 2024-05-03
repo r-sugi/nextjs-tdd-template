e19e72eca1d11f305064d7214475e25cb1194632
r-sugi <r.sugimoto0615@gmail.com>
2024-05-04 00:06:54 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,6 @@
 const nextConfig = {
   pageExtensions: ['page.tsx'],
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
