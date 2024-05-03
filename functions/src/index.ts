78ab8665d983e016eb7502f199044405354120ee
r-sugi <r.sugimoto0615@gmail.com>
2024-05-04 02:21:03 +0900

    add: success to deploy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,56 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+/* eslint @typescript-eslint/no-explicit-any: "off" */
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const { info, error } = require('firebase-functions/logger');
+const { initializeApp, auth, firestore } = require('firebase-admin/app');
+const { onRequest } = require('firebase-functions/v2/https');
+const functionsV1 = require('firebase-functions/v1');
 
-import { onRequest } from 'firebase-functions/v2/https';
-import * as logger from 'firebase-functions/logger';
+initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = onRequest((request, response) => {
-  logger.info('Hello logs!', { structuredData: true });
+export const helloWorld = onRequest((request: any, response: any) => {
+  info('Hello logs!', { structuredData: true });
   response.send('Hello from Firebase!');
+});
+
+const createUser = `
+mutation createUser($id: String = "", $email: String = "") {
+  insert_users_one(object: {id: $id, email: $email}, on_conflict: {constraint: users_pkey, update_columns: []}) {
+    id
+    email
+  }
+}
+`;
+
+export const authUser = functionsV1.auth.user().onCreate((user: any) => {
+  const customClaims = {
+    'https://hasura.io/jwt/claims': {
+      'x-hasura-default-role': 'user',
+      'x-hasura-allowed-roles': ['user'],
+      'x-hasura-user-id': user.uid,
+    },
+  };
+
+  return auth()
+    .setCustomUserClaims(user.uid, customClaims)
+    .then(() => {
+      const queryStr = {
+        query: createUser,
+        variables: { id: user.uid, email: user.email },
+      };
+
+      fetch('<url>', {
+        method: 'post',
+        body: JSON.stringify(queryStr),
+        headers: {
+          'x-hasura-admin-secret': '<secret>',
+        },
+      });
+
+      firestore().collection('user_meta').doc(user.uid).create({
+        refreshTime: firestore.FieldValue.serverTimestamp(),
+      });
+    })
+    .catch((e: any) => {
+      error(e);
+    });
 });
