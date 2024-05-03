8e034c2ff1f8d8d87d843029e1e4be84eb769eb9
r-sugi <r.sugimoto0615@gmail.com>
2024-05-03 23:26:10 +0900

    fix: unused code

--- src/feature/mypage/index/index.tsx
+++ src/feature/mypage/index/index.tsx
@@ -3,7 +3,6 @@ import { FC } from 'react';
 
 import { useFetchActiveMember } from '@/core/usecases/member/useFetchActiveMember.query';
 
-
 export const IndexTemplate: FC = () => {
   const router = useRouter();
   const { data: activeMember, loading } = useFetchActiveMember({
@@ -17,10 +16,4 @@ export const IndexTemplate: FC = () => {
   }
 
   return <div>ActiveMember: {JSON.stringify(activeMember, null, 2)}</div>;
-};
-
-export const PostIdTemplate: FC = () => {
-  return (
-      <IndexTemplate />
-  );
 };
