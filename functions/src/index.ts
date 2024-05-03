56b3255be406014b979b9a151a671a235a7cd2f4
r-sugi <r.sugimoto0615@gmail.com>
2024-05-04 02:35:56 +0900

    add: success to deploy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,15 @@
-/* eslint @typescript-eslint/no-explicit-any: "off" */
-/* eslint @typescript-eslint/no-var-requires: "off" */
-const { info, error } = require('firebase-functions/logger');
 const { initializeApp, auth, firestore } = require('firebase-admin/app');
-const { onRequest } = require('firebase-functions/v2/https');
+const { info, error } = require('firebase-functions/logger');
 const functionsV1 = require('firebase-functions/v1');
 
 initializeApp();
 
-export const helloWorld = onRequest((request: any, response: any) => {
-  info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+export const helloWorld = functionsV1
+  .region('asia-northeast1')
+  .https.onRequest((request: any, response: any) => {
+    info('Hello logs!', { structuredData: true });
+    response.send('Hello from Firebase!');
+  });
 
 const createUser = `
 mutation createUser($id: String = "", $email: String = "") {
@@ -21,36 +20,39 @@ mutation createUser($id: String = "", $email: String = "") {
 }
 `;
 
-export const authUser = functionsV1.auth.user().onCreate((user: any) => {
-  const customClaims = {
-    'https://hasura.io/jwt/claims': {
-      'x-hasura-default-role': 'user',
-      'x-hasura-allowed-roles': ['user'],
-      'x-hasura-user-id': user.uid,
-    },
-  };
+export const authUser = functionsV1
+  .region('asia-northeast1')
+  .auth.user()
+  .onCreate((user: any) => {
+    const customClaims = {
+      'https://hasura.io/jwt/claims': {
+        'x-hasura-default-role': 'user',
+        'x-hasura-allowed-roles': ['user'],
+        'x-hasura-user-id': user.uid,
+      },
+    };
 
-  return auth()
-    .setCustomUserClaims(user.uid, customClaims)
-    .then(() => {
-      const queryStr = {
-        query: createUser,
-        variables: { id: user.uid, email: user.email },
-      };
+    return auth()
+      .setCustomUserClaims(user.uid, customClaims)
+      .then(() => {
+        const queryStr = {
+          query: createUser,
+          variables: { id: user.uid, email: user.email },
+        };
 
-      fetch('<url>', {
-        method: 'post',
-        body: JSON.stringify(queryStr),
-        headers: {
-          'x-hasura-admin-secret': '<secret>',
-        },
+        fetch('<url>', {
+          method: 'post',
+          body: JSON.stringify(queryStr),
+          headers: {
+            'x-hasura-admin-secret': '<secret>',
+          },
+        });
+
+        firestore().collection('user_meta').doc(user.uid).create({
+          refreshTime: firestore.FieldValue.serverTimestamp(),
+        });
+      })
+      .catch((e: any) => {
+        error(e);
       });
-
-      firestore().collection('user_meta').doc(user.uid).create({
-        refreshTime: firestore.FieldValue.serverTimestamp(),
-      });
-    })
-    .catch((e: any) => {
-      error(e);
-    });
-});
+  });
