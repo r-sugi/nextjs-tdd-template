59f293bec604c0dec3182af1c0d0701616387d04
r-sugi <r.sugimoto0615@gmail.com>
2024-06-06 15:21:00 +0900

    fix: format, lint

--- knip.js
+++ knip.js
@@ -1,27 +1,28 @@
 module.exports = {
-  ignore: [
-    '**/*.d.ts',
-    // Unused files
-    'mocks/browser.ts',
-    'mocks/index.ts',
-    'mocks/server.ts',
-    'mocks/rest/restHandlers.ts',
-    'public/mockServiceWorker.js',
-    'src/error/errors/unhandledRejectionError.ts',
-    'src/__todo/PostIdErrorBoundary.tsx',
-    'src/__todo/PostIdErrorScreen.tsx',
-    // Unused exports, Unused exported types of component templates
-    'src/components/**/*.*',
-  ],
-  ignoreWorkspaces: ['src/generated/**'],
-  ignoreDependencies: [
-    // Unused devDependencies
-    'next-router-mock',
-    'msw',
-    'jest-environment-jsdom',
-    'eslint-import-resolver-typescript',
-    // Unlisted dependencies
-    'dotenv',
-  ],
-  ignoreBinaries: ['docker-compose', 'npm-run-all'],
+	ignore: [
+		"**/*.d.ts",
+		// Unused files
+		"mocks/browser.ts",
+		"mocks/index.ts",
+		"mocks/server.ts",
+		"mocks/rest/restHandlers.ts",
+		"public/mockServiceWorker.js",
+		"src/error/errors/unhandledRejectionError.ts",
+		"src/__todo/PostIdErrorBoundary.tsx",
+		"src/__todo/PostIdErrorScreen.tsx",
+		// Unused exports, Unused exported types of component templates
+		"src/components/**/*.*",
+		"biome.js",
+	],
+	ignoreWorkspaces: ["src/generated/**"],
+	ignoreDependencies: [
+		// Unused devDependencies
+		"next-router-mock",
+		"msw",
+		"jest-environment-jsdom",
+		"eslint-import-resolver-typescript",
+		// Unlisted dependencies
+		"dotenv",
+	],
+	ignoreBinaries: ["docker-compose", "npm-run-all"],
 };
