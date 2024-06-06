b0bd2bb29ea33ecb5c2578e2f033592d55e293a6
r-sugi <r.sugimoto0615@gmail.com>
2024-06-06 22:32:45 +0900

    add: errorLink

--- src/pages/_provider/_appApollo.provider.tsx
+++ src/pages/_provider/_appApollo.provider.tsx
@@ -5,6 +5,7 @@ import {
 	InMemoryCache,
 	from,
 } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 import { type FC, type ReactNode, useMemo } from "react";
 
 import { NEXT_PUBLIC_GRAPHQL_URI } from "@/config/env";
@@ -12,13 +13,34 @@ import { NEXT_PUBLIC_GRAPHQL_URI } from "@/config/env";
 export const AppApolloProvider: FC<{
 	children: ReactNode;
 }> = ({ children }) => {
+	const errorLink = useMemo(
+		() =>
+			onError(({ operation, graphQLErrors, networkError }) => {
+				if (graphQLErrors) {
+					for (const { message, extensions } of graphQLErrors) {
+						console.log(
+							`[GraphQL error]: Message: ${message}, Code: ${extensions.code}, Path: ${extensions.path}, Operation: ${operation}`,
+						);
+					}
+				}
+				if (networkError) {
+					console.log(
+						`[Network error]: ${networkError}, Operation: ${operation}`,
+					);
+				}
+			}),
+		[],
+	);
+
 	const link = useMemo(() => {
 		return from([
+			errorLink,
 			new HttpLink({
 				uri: NEXT_PUBLIC_GRAPHQL_URI,
 			}),
 		]);
-	}, []);
+	}, [errorLink]);
+
 	const client = useMemo(
 		() =>
 			new ApolloClient({
