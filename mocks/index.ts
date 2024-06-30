523cf697caf65b932b7c9bdf33a3ad47b0e5a472
r-sugi <r.sugimoto0615@gmail.com>
2024-06-30 12:37:02 +0900

    fix: type-check

--- mocks/index.ts
+++ mocks/index.ts
@@ -1,11 +1,12 @@
 async function initMocks() {
-  if (typeof window === "undefined") {
-    const { server } = await import("./server");
-    server.listen();
-  } else {
-    const { worker } = await import("./browser");
-    worker.start({ onUnhandledRequest: "bypass" });
-  }
+	if (typeof window === "undefined") {
+		const { server } = await import("./server");
+		server.listen();
+	} else {
+		const { worker } = await import("./browser");
+		worker.start({ onUnhandledRequest: "bypass" });
+	}
 }
 initMocks();
+// biome-ignore lint/style/useExportType: <explanation>
 export {};
