0439374f3289df68449fe0dfb8f042e124e5472d
r-sugi <r.sugimoto0615@gmail.com>
2024-07-15 18:25:09 +0900

    add: local storycap take __screenshots__

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,9 @@
 import "./globals.css";
 import type { Preview } from "@storybook/react";
+import { withScreenshot } from "storycap";
 import "tailwindcss/tailwind.css";
+
+export const decorators = [withScreenshot];
 
 const preview: Preview = {
 	parameters: {
@@ -8,6 +11,16 @@ const preview: Preview = {
 			matchers: {
 				color: /(background|color)$/i,
 				date: /Date$/i,
+			},
+		},
+		screenshot: {
+			// see: https://github.com/reg-viz/storycap?tab=readme-ov-file#type-variants
+			fullPage: false,
+			delay: 0,
+			viewports: {
+				desktop: { width: 1024, height: 768 },
+				tablet: { width: 375, height: 668 },
+				mobile: { width: 320, height: 568, isMobile: true, hasTouch: true },
 			},
 		},
 	},
