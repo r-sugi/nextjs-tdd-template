1c49a9ce8400ac681415edde9bbcbb3d5f3f8281
r-sugi <r.sugimoto0615@gmail.com>
2024-08-22 13:01:34 +0900

    wip: 1回目の描画が残っている

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,3 +2,27 @@ import "@testing-library/jest-dom";
 
 // TODO: 不要なら削除したい
 import "whatwg-fetch";
+
+jest.mock("next/router", () => ({
+	useRouter() {
+		return {
+			route: "/",
+			pathname: "/",
+			query: {},
+			asPath: "/",
+			basePath: "",
+			push: jest.fn().mockResolvedValue(true),
+			replace: jest.fn().mockResolvedValue(true),
+			reload: jest.fn(),
+			back: jest.fn(),
+			prefetch: jest.fn().mockResolvedValue(undefined),
+			beforePopState: jest.fn(),
+			events: {
+				on: jest.fn(),
+				off: jest.fn(),
+				emit: jest.fn(),
+			},
+			isFallback: false,
+		};
+	},
+}));
--- src/feature/mypage/resignMember/input/index.test.tsx
+++ src/feature/mypage/resignMember/input/index.test.tsx
@@ -1,4 +1,68 @@
-// TODO: mockだらけにならないようにmswを使ったブラックボックステストにする
-describe.skip("test", () => {
-	it.skip("test", () => {});
+import { toMock, toSpyWithMock } from "@/__testing__/helper";
+import { loginRequiredPages } from "@/const/paths";
+import { setCache } from "@/utils/cache";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import * as router from "next/router";
+import IndexTemplate from "./index";
+
+jest.mock("@/utils/cache");
+
+describe("IndexTemplate", () => {
+	const routerMock = jest.fn();
+
+	afterEach(() => {
+		jest.resetAllMocks();
+		jest.restoreAllMocks();
+	});
+
+	function setup() {
+		return {
+			user: userEvent.setup(),
+			view: render(<IndexTemplate />),
+		};
+	}
+
+	describe("正常系", () => {
+		describe("初期描画が変化していない", () => {
+			const { view } = setup();
+			expect(view.container).toMatchSnapshot();
+		});
+
+		describe("フォーム送信に成功する", () => {
+			it("キャッシュに保存して、ページ遷移する", async () => {
+				// Arrange
+				const mockSetCache = toMock(setCache);
+				const useRouterMockImplementation = () => ({
+					push: routerMock,
+				});
+				toSpyWithMock(router, "useRouter", useRouterMockImplementation);
+
+				// Act
+				const { user, view } = setup();
+
+				const reasonTypeSelectBox = await view.findByLabelText("退会理由");
+				const reasonDetailInput = await view.findByLabelText("詳細");
+				const agreeementCheckBox = await view.findByLabelText("同意する");
+				await user.selectOptions(reasonTypeSelectBox, "その他");
+				await user.type(reasonDetailInput, "使わないため");
+				await user.click(agreeementCheckBox);
+
+				const submitButton = await view.findByRole("button", {
+					name: "退会する",
+				});
+				await user.click(submitButton);
+
+				// Assert
+				expect(mockSetCache).toHaveBeenCalledWith("resignMember", {
+					reasonType: "OTHER",
+					reasonDetail: "使わないため",
+					agreement: true,
+				});
+				expect(routerMock).toHaveBeenCalledWith(
+					loginRequiredPages.mypageResignMemberConfirm.path(),
+				);
+			});
+		});
+	});
 });
