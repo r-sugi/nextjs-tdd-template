f40d8540e234ec0800df998e7790709a638fc711
r-sugi <r.sugimoto0615@gmail.com>
2024-08-22 11:28:00 +0900

    refactor: form labelに型をつける

--- src/feature/mypage/resignMember/hooks/form/index.ts
+++ src/feature/mypage/resignMember/hooks/form/index.ts
@@ -1,14 +1,22 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { type infer as ZodInfer, z } from "zod";
+import { type infer as ZodInfer, type ZodSchema, z } from "zod";
 
 import { agreement, reasonDetail, reasonType } from "./validation";
+
+type SchemaKey = "reasonType" | "reasonDetail" | "agreement";
+
+const resignMemberLabels = {
+	reasonType: "退会理由",
+	reasonDetail: "詳細",
+	agreement: "同意する",
+} as const satisfies Record<SchemaKey, string>;
 
 const resignMemberSchema = z.object({
 	reasonType: reasonType(),
 	reasonDetail: reasonDetail(),
 	agreement: agreement(),
-});
+} satisfies Record<SchemaKey, ZodSchema>);
 
 export type ResignMemberSchema = ZodInfer<typeof resignMemberSchema>;
 
@@ -38,5 +46,6 @@ export const useResignMemberForm = (props?: Props) => {
 		register,
 		formState: { isSubmitting, isValid, errors },
 		setError,
+		resignMemberLabels,
 	};
 };
