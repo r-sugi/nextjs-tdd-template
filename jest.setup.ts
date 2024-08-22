c293ec0cf88b0bc0ae57c1b0461d40ade82932a8
r-sugi <r.sugimoto0615@gmail.com>
2024-08-22 18:30:58 +0900

    add: data-testidを明示的に指定する

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,8 @@
 import "@testing-library/jest-dom";
 
 jest.mock("next/router", () => jest.requireActual("next-router-mock"));
+
+import { configure } from "@testing-library/react";
+configure({
+	testIdAttribute: "data-testid",
+});
