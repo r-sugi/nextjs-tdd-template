ce42edefff7b3bca303631456e9453983d392973
r-sugi <r.sugimoto0615@gmail.com>
2024-09-04 21:11:12 +0900

    add: as const satisfies にする

--- mocks/fixtures/activeMember.ts
+++ mocks/fixtures/activeMember.ts
@@ -1,12 +1,12 @@
-import { ActiveMember } from '@/core/domains/member/activeMember';
+import type { ActiveMember } from "@/core/domains/member/activeMember";
 
-export const activeMember: ActiveMember = {
-  status: 'active',
-  statusActivityId: '1',
-  memberId: 'xxx-eeee-ddddd',
-  address: 'Tokyo',
-  postalCode: '100-0001',
-  email: 'hoge@example.com',
-  birthday: new Date('1987-05-01'),
-  createdAt: new Date('2024-05-01'),
-};
+export const activeMember = {
+	status: "active",
+	statusActivityId: "1",
+	memberId: "xxx-eeee-ddddd",
+	address: "Tokyo",
+	postalCode: "100-0001",
+	email: "hoge@example.com",
+	birthday: new Date("1987-05-01"),
+	createdAt: new Date("2024-05-01"),
+} as const satisfies ActiveMember;
