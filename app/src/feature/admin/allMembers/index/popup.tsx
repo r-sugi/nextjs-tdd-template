a5967035e239661e4c4b10960e4d28f1f2ca8291
r-sugi <r.sugimoto0615@gmail.com>
2024-09-16 21:20:35 +0900

    fix

--- app/src/feature/admin/allMembers/index/popup.tsx
+++ app/src/feature/admin/allMembers/index/popup.tsx
@@ -1,49 +1,49 @@
 import { type PropsWithChildren, useState } from "react";
 
 type Props = {
-  opener: React.ReactNode;
+	opener: React.ReactNode;
 };
 
 export const PopupMenu = ({ opener, children }: PropsWithChildren<Props>) => {
-  const [visible, setVisible] = useState(false);
-  const onOpen = () => {
-    setVisible(true);
-  };
+	const [visible, setVisible] = useState(false);
+	const onOpen = () => {
+		setVisible(true);
+	};
 
-  const onClose = () => {
-    setVisible(false);
-  };
+	const onClose = () => {
+		setVisible(false);
+	};
 
-  return (
-    <div className="relative">
-      <button
-        id="user-menu"
-        type="button"
-        aria-haspopup="true"
-        onMouseDown={onOpen}
-      >
-        {opener}
-      </button>
+	return (
+		<div className="relative">
+			<button
+				id="user-menu"
+				type="button"
+				aria-haspopup="true"
+				onMouseDown={onOpen}
+			>
+				{opener}
+			</button>
 
-      {visible && (
-        // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
-        <div
-          id="overlay"
-          className="fixed inset-0 z-0 bg-transparent flex justify-center items-center"
-          onClick={onClose}
-        >
-          <div
-            id="user-menu-dropdown"
-            className={`absolute z-50 bg-white right-0 w-48 mt-2 origin-top-right rounded-lg shadow-lg transform ${
-              visible
-                ? "scale-100 opacity-100 ease-in duration-100"
-                : "scale-0 opacity-0 ease-out duration-75"
-            } top-13`}
-          >
-            {children}
-          </div>
-        </div>
-      )}
-    </div>
-  );
+			{visible && (
+				// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+				<div
+					id="overlay"
+					className="fixed inset-0 z-0 bg-transparent flex justify-center items-center"
+					onClick={onClose}
+				>
+					<div
+						id="user-menu-dropdown"
+						className={`absolute z-50 bg-white right-0 w-48 mt-2 origin-top-right rounded-lg shadow-lg transform ${
+							visible
+								? "scale-100 opacity-100 ease-in duration-100"
+								: "scale-0 opacity-0 ease-out duration-75"
+						} top-13`}
+					>
+						{children}
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
