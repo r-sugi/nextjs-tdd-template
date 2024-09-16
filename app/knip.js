eb21bfe30a493dfe9b742f9cc742eb5e6f5967a0
r-sugi <r.sugimoto0615@gmail.com>
2024-09-17 04:38:19 +0900

    fix: unused

--- app/knip.js
+++ app/knip.js
@@ -3,18 +3,12 @@ module.exports = {
 		"**/*.d.ts",
 		// Unused files
 		"src/__mocks__/**/*.*",
-		"functions/src/index.ts",
 		"mocks/**/*.ts",
 		"public/mockServiceWorker.js",
-		"src/error/errors/unhandledRejectionError.ts",
 		"postcss.config.js",
 		// Unused exports, Unused exported types of component templates
 		"src/components/**/*.*",
 		"biome.js",
-		"functions/lib/**",
-		"functions/.bin/**",
-		"functions/src/index.ts",
-		"src/api/articles/[id]/index.ts",
 		"src/mocks/fixtures/member/allMembers.ts",
 	],
 	ignoreWorkspaces: ["src/generated/**"],
@@ -23,10 +17,9 @@ module.exports = {
 		"next-router-mock",
 		"jest-environment-jsdom",
 		"pino-pretty",
-		"lefthook",
 		// Unlisted dependencies
 		"dotenv",
 		"ts-node",
 	],
-	ignoreBinaries: ["docker-compose", "sed"],
+	ignoreBinaries: ["sed"],
 };
