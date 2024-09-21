ff721ecf6c574e0491363d4cef04f81b8ad66061
r-sugi <r.sugimoto0615@gmail.com>
2024-09-21 16:34:18 +0900

    fix: functions deploy, functions container

---
+++ functions/biome.js
@@ -0,0 +1,48 @@
+module.exports = {
+	$schema: "https://biomejs.dev/schemas/1.8.0/schema.json",
+	vcs: {
+		enabled: true,
+		clientKind: "git",
+		useIgnoreFile: true,
+	},
+	organizeImports: {
+		enabled: true,
+	},
+	rules: {
+		useImportType: "off",
+	},
+	formatter: {
+		enabled: true,
+		include: ["src/**"],
+		indentWidth: 2,
+		indentStyle: "space",
+		lineWidth: 100,
+	},
+	javascript: {
+		parser: {
+			unsafeParameterDecoratorsEnabled: true,
+		},
+		formatter: {
+			enabled: true,
+			quoteStyle: "single",
+			jsxQuoteStyle: "single",
+			semicolons: "always",
+			arrowParentheses: "always",
+			quoteProperties: "asNeeded",
+		},
+	},
+	linter: {
+		enabled: true,
+		include: ["src/**"],
+		rules: {
+			recommended: true,
+			style: {
+				useSelfClosingElements: "error",
+			},
+			suspicious: {
+				noDebugger: "error",
+				noConsoleLog: "error",
+			},
+		},
+	},
+};
